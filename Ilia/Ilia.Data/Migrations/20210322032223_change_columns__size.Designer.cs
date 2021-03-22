fcf662efb2dd27dbc91e15e7b28f31038af6f9a1
Lucas da Silva Bomfonti <lucas.bomfonti@ambevtech.com.br>
2021-03-22 00:23:36 -0300

    changed migrations.

--- Ilia/Ilia.Data/Migrations/20210322032223_change_columns__size.Designer.cs
+++ Ilia/Ilia.Data/Migrations/20210322032223_change_columns__size.Designer.cs
@@ -4,14 +4,16 @@ using Ilia.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ilia.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210322032223_change_columns__size")]
+    partial class change_columns__size
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ilia/Ilia.Data/Migrations/20210322032223_change_columns__size.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ilia.Data.Migrations
+{
+    public partial class change_columns__size : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "User",
+                type: "VARCHAR(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(40)",
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "VARCHAR(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(40)",
+                oldMaxLength: 40);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "User",
+                type: "VARCHAR(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "VARCHAR(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
